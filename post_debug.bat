3b36c7ce34674e4dd8429cc7f5c7abfb2d5e57b8
WarStalkeR <stalker6000@hotmail.com>
2023-01-05 14:20:17 +0200

    License update. Deprecated files removal.

--- post_debug.bat
+++
@@ -1,3 +0,0 @@
-set sourcefile=%~1
-set targetdir=%~2
-echo F | xcopy /F /Y "%sourcefile%" "%targetdir%..\..\BepInEx\monomod\WTF.FFU_TL.mm.dll"
--- post_release.bat
+++
@@ -1,8 +0,0 @@
-set sourcefile=%~1
-set targetdir=%~2
-echo F | xcopy /F /Y "%sourcefile%" "%targetdir%..\..\WTF.FFU_TL.mm.dll"
-waitfor SomethingThatIsNeverHappening /t 2 2>NUL
-"%targetdir%..\MonoMod\MonoMod.exe" "%targetdir%..\..\WTF.exe"
-del "%targetdir%..\..\WTF.FFU_TL.mm.dll" /F
-del "%targetdir%..\..\MONOMODDED_WTF.pdb" /F
-move /Y "%targetdir%..\..\MONOMODDED_WTF.exe" "%targetdir%..\..\WTF_modded.exe"
